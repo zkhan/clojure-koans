b8c0546804cd1ffb684da9f208eeede45cbba9fd
Zubair Khan <zkhan@glass-i.com>
2011-06-05 23:14:45 +0200

    Recursion done, it hurts, I must do more

--- src/koans/recursion.clj
+++ src/koans/recursion.clj
@@ -1,20 +1,25 @@
 (defn is-even? [n]
   (if (= n 0)
-      __
-      (___ (is-even? (dec n)))))
+      true
+      (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-        __
+        acc
         (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= 1 (count coll))
+    coll
+    (cons (last coll) (recursive-reverse (pop coll)))))
 
 (defn factorial [n]
-  __)
+  (loop [value n acc 1]
+    (if (zero? value)
+      acc
+      (recur (dec value) (* acc value)))))
 
 (meditations
   "Recursion ends with a base case"
